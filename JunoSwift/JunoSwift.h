a35327b17d56da2f313d740aa04135dab365ec71
Uğur Uğurlu <ugurugurlu@ogoodigital.com>
2022-05-24 23:43:23 +0300

    project created

---
+++ JunoSwift/JunoSwift.h
@@ -0,0 +1,18 @@
+//
+//  JunoSwift.h
+//  JunoSwift
+//
+//  Created by Uğur Uğurlu on 24.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JunoSwift.
+FOUNDATION_EXPORT double JunoSwiftVersionNumber;
+
+//! Project version string for JunoSwift.
+FOUNDATION_EXPORT const unsigned char JunoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JunoSwift/PublicHeader.h>
+
+
